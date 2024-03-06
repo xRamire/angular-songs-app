5e305ea1865019becf6762eaf110683c38acc9eb
xRamire <ramiire@gmail.com>
2024-03-06 21:34:21 +0100

    defining structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SongListComponent } from './components/songs/song-list/song-list.component';
+import { SongDetailsComponent } from './components/songs/song-details/song-details.component';
+import { NavbarComponent } from './components/navigation/navbar/navbar.component';
+import { SidebarComponent } from './components/navigation/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongListComponent,
+    SongDetailsComponent,
+    NavbarComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule
